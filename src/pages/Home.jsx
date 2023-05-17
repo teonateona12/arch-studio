f78664a1f117d6485be76c467203188d7db315cb
teonateona12 <teona.piranishvili1@gmail.com>
2023-05-17 17:50:45 +0400

    style: work with home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,24 @@ import Header from "../components/Header.jsx";
 import ProjectMobile from "../assets/home/mobile/image-hero-paramour.jpg";
 import ProjectTablet from "../assets/home/tablet/image-hero-paramour.jpg";
 import ProjectDesktop from "../assets/home/desktop/image-hero-paramour.jpg";
-
 import Arrow from "../assets/icons/icon-arrow.svg";
+import Welcome from "../assets/home/desktop/image-welcome.jpg";
+import TeamMobile from "../assets/home/mobile/image-small-team.jpg";
+import TeamTablet from "../assets/home/tablet/image-small-team.jpg";
+import TeamDesktop from "../assets/home/desktop/image-small-team.jpg";
+
+import OneMobile from "../assets/portfolio/mobile/image-prototype.jpg";
+import OneTablet from "../assets/portfolio/tablet/image-prototype.jpg";
+import OneDesktop from "../assets/portfolio/desktop/image-prototype.jpg";
+
+import TwoMobile from "../assets/portfolio/mobile/image-228b.jpg";
+import TwoTablet from "../assets/portfolio/tablet/image-228b.jpg";
+import TwoDesktop from "../assets/portfolio/desktop/image-228b.jpg";
+
+import ThreeMobile from "../assets/portfolio/mobile/image-del-sol.jpg";
+import ThreeTablet from "../assets/portfolio/tablet/image-del-sol.jpg";
+import ThreeDesktop from "../assets/portfolio/desktop/image-del-sol.jpg";
+
 export default function Home() {
   return (
     <div>
@@ -25,6 +41,77 @@ export default function Home() {
           <div className="button-div">
             <button>See Our Portfolio</button>
             <img src={Arrow} />
+          </div>
+        </div>
+      </div>
+      <div className="welcome">
+        <div>
+          <h3>Welcome to Arch Studio</h3>
+          <p>
+            We have a unique network and skillset to help bring your projects to
+            life. Our small team of highly skilled individuals combined with our
+            large network put us in a strong position to deliver exceptional
+            results. <br /> <br /> Over the past 10 years, we have worked on all
+            kinds of projects. From stations to high-rise buildings, we create
+            spaces that inspire and delight. <br />
+            <br /> We work closely with our clients so that we understand the
+            intricacies of each project. This allows us to work in harmony the
+            surrounding area to create truly stunning projects that will stand
+            the test of time.
+          </p>
+        </div>
+        <img src={Welcome} />
+      </div>
+      <div className="project">
+        <img className="mobile" src={TeamMobile} />
+        <img className="tablet" src={TeamTablet} />
+        <img className="desktop" src={TeamDesktop} />
+        <div className="text-div">
+          <h1>
+            Small team, <br />
+            big ideas
+          </h1>
+          <div className="button-div">
+            <button>About Us</button>
+            <img src={Arrow} />
+          </div>
+        </div>
+      </div>
+      <div className="featured">
+        <div className="featured-text-div">
+          <h3>Featured</h3>
+          <div className="button-div">
+            <p>See All</p>
+            <img src={Arrow} />
+          </div>
+        </div>
+        <div className="images-div">
+          <div className="one-text-image">
+            <img className="one-mobile" src={OneMobile} />
+            <img className="one-tablet" src={OneTablet} />
+            <img className="one-desktop" src={OneDesktop} />
+            <div>
+              <h3>Project Del Sol</h3>
+              <button>View All Projects</button>
+            </div>
+          </div>
+          <div className="one-text-image">
+            <img className="two-mobile" src={TeamMobile} />
+            <img className="two-tablet" src={TwoTablet} />
+            <img className="two-desktop" src={TwoDesktop} />
+            <div>
+              <h3>Project Del Sol</h3>
+              <button>View All Projects</button>
+            </div>
+          </div>
+          <div className="one-text-image">
+            <img className="three-mobile" src={ThreeMobile} />
+            <img className="three-tablet" src={ThreeTablet} />
+            <img className="three-desktop" src={ThreeDesktop} />
+            <div>
+              <h3>Project Del Sol</h3>
+              <button>View All Projects</button>
+            </div>
           </div>
         </div>
       </div>
